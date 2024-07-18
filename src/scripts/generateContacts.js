5ad33b39ce9df6ea61f0ca36f643afa08cf661eb
Demian Pashkovskyi <95377702+Demian-Pashkovskyi@users.noreply.github.com>
2024-07-18 23:34:17 +0300

    generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,28 @@
+import * as fs from "node:fs/promises"; 
 import { PATH_DB } from '../constants/contacts.js';
+import 	{ createFakeContact } from "../utils/createFakeContact.js";
 
-const generateContacts = async (number) => {};
 
-generateContacts(5);
+
+const generateContacts = async (number) => {
+try {
+	const data = await fs.readFile(PATH_DB, "utf-8");
+	const contacts = JSON.parse(data);
+
+	const newContacts = [];
+	for (let i = 0; i < number; i++) {
+		newContacts.push(createFakeContact());
+	}
+
+	const updatedContacts = [...contacts, ...newContacts];
+
+	await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+
+	console.log("Successfully added  ${number} new contacts.");
+
+} catch ( error ) {
+	console.error("Error", error);
+}
+};
+
+await generateContacts(5);
