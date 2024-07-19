3d1cc7ac7bfb2376fdfc2ab9cf836f062ca7b02f
Demian Pashkovskyi <95377702+Demian-Pashkovskyi@users.noreply.github.com>
2024-07-19 22:38:32 +0300

    countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,7 +1,17 @@
-import fs from
-
+import * as fs from "node:fs/promises"; 
 import { PATH_DB } from '../constants/contacts.js';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+	try {
+		const data = await fs.readFile(PATH_DB, "utf-8");
+		const contacts = JSON.parse(data);
+
+		return contacts.lenght;	
+	} catch (error) {
+		console.error("Error", error);
+		return 0;
+	}
+
+};
 
 console.log(await countContacts());
