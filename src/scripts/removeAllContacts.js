d661233b5a9f746135d3ba518ff5409db03f561a
Demian Pashkovskyi <95377702+Demian-Pashkovskyi@users.noreply.github.com>
2024-07-19 23:14:32 +0300

    add-one

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,14 @@
+import * as fs from "node:fs/promises"; 
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+	try {
+		await fs.writeFile(PATH_DB, JSON.stringify([], null, 2));
+
+		console.log("Successfully removed all contacts.");
+	} catch (error) {
+		console.error("Error", error);
+	}
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,8 @@
+import * as fs from "node:fs/promises"; 
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+	await fs.writeFile(PATH_DB, []);
+};
 
 removeLastContact();
